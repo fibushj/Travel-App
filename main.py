5513ef1232b410cee9120c0fd6b0e28f40513975
fibushj <jfibush@gmail.com>
2021-01-07 17:07:31 +0200

    refactoring

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
+from models.model import Model
 from gui.gui_run import MainGUI
 from models.initialization.initialization import initialize_db
-import mysql.connector
 from models.config import *
 
 
 # TODO JHONNY: Database should be the only thing that modifies the database. When the GUI needs to send information to, or get information from the database, it can call methods on the database.
+model = Model()
+#model.initialize()
 database = NotImplemented
 gui = MainGUI(database)
 gui.run()
-
-# 
-# #This code creates connection to MySql, and creates new data base for project
+model.close()
 
 
-# mydb = mysql.connector.connect(
-#     option_files='my.conf',
-#     autocommit=True
-# )
-# cursor = mydb.cursor()
-# cursor.execute('SET sql_mode = ""')
 
-# initialize_db(cursor)
-
-# cursor.close()
-
-# mydb.close()
