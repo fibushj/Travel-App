e53273ad22a4fbe97c6da97ce639a0f1a32bfd41
Anton <didchenkoanton94@gmail.com>
2021-01-11 13:25:37 +0200

    Updated error handling code in utils.py (controller)

--- controller/utils.py
+++ controller/utils.py
@@ -3,5 +3,13 @@ def generateErrorMessage(error_number):
         return "You already wrote review for such trip"
     if error_number == 1048:
         return "Incorrect query arguments"
+    if error_number == 1045:
+        return "Access denied for current client credentials"
+    if error_number == 2013:
+        return "Lost connection with server"
+    if error_number == 2002:
+        return "Cannot conect with server"
+    if error_number == 2008 or error_number == 1114:
+        return "Server ran out of memory"
     else:
-        return f"Unknown error code:{error_number}" 
+        return f"Raised Mysql error:{error_number}" 
